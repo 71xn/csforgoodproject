b5754d5744aec2a98b002cc4fe5c13f5b6c253db
Finn <44287141+71xn@users.noreply.github.com>
2021-04-15 21:27:49 +0100

    updated code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryLabel } from 'victory';
+import Data from "./d2.json";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const data1 = Data;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//const newdata = Data.map(data => data.PL == "NOx");
+
+class App extends React.Component {
+  render() {
+    return (
+      <VictoryChart
+        // domainPadding will add space to each side of VictoryBar to
+        // prevent it from overlapping the axis
+        domainPadding={20}
+      >
+
+      <VictoryAxis
+          tickValues={1990, 1995, 2000, 2005, 2010, 2015}
+          label="Year"
+        />
+        <VictoryAxis
+          dependentAxis
+          //tickValues={500, 1000, 1500, 2000, 2500, 3000, 3500}
+          label="Pollutant Amount (1000 tonnes)"
+          //style={{axisLabel: {padding: 35 } }} 
+        />
+
+        <VictoryBar
+          data={data1}
+          x="Year"
+          y="Emissions"
+        />
+      </VictoryChart>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
