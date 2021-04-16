610f2c77dff82c0e5002bfccec9f958fcfb5b663
Finn <44287141+71xn@users.noreply.github.com>
2021-04-16 22:33:56 +0100

    fixed spelling

--- src/DS.js
+++ src/DS.js
@@ -3,7 +3,7 @@
 // Takes params of user selection and the JSON Data object
  
 function sortData(selection, Data) {
-    // https://www.stechies.com/unexpected-token-o-json-position-1/ - Helped to fox the JSON parsing issue for sorting data
+    // https://www.stechies.com/unexpected-token-o-json-position-1/ - Helped to fix the JSON parsing issue for sorting data
     var data1 = JSON.stringify(Data);
     var selectionOnly = JSON.parse(data1).filter(function (entry) {
         return entry.PL === selection;
